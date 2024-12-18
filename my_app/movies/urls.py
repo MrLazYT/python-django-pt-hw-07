7d8aaed3e6cd579c9490d36c3572ea169257958d
Denys Gladan <69466074+MrLazYT@users.noreply.github.com>
2024-12-18 21:43:53 +0200

    Added Create Page

--- my_app/movies/urls.py
+++ my_app/movies/urls.py
@@ -5,5 +5,6 @@ from movies import views
 urlpatterns = [
     path('', views.list),
     path('details/<id>', views.details),
+    path('create', views.create),
     path('delete/<id>', views.delete)
 ]
--- my_app/movies/views.py
+++ my_app/movies/views.py
@@ -2,11 +2,25 @@ from django.shortcuts import render, redirect
 
 from movies.models import Movie
 
+from movies.forms import MovieForm
+
+
 # Create your views here.
 def list(request):
     movies = Movie.objects.all()
 
     return render(request, 'list.html', {'movies': movies})
+
+def create(request):
+    form = MovieForm(request.POST)
+
+    if (request.method == 'POST'):
+        if (form.is_valid()):
+            form.save()
+
+            return redirect('/')
+
+    return render(request, 'create.html', {'form': form})
 
 def details(request, id):
     movie = Movie.objects.get(id=id)
